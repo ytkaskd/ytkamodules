a38ce060c2bfcb07b3652d30446842a49addd00a
stack overdose <95956785+ytkaskd@users.noreply.github.com>
2024-04-21 22:42:17 +0300

    Create randomfact.py

---
+++ randomfact.py
@@ -0,0 +1,25 @@
+from hikkatl.types import Message
+from .. import loader, utils
+
+import requests
+from bs4 import BeautifulSoup # type: ignore
+
+
+def get_fact():
+        try:
+            response = requests.get('https://randstuff.ru/fact/')
+            page = BeautifulSoup(response.content, 'html.parser')
+            fact = page.find('table', {'class': 'text'}).get_text(strip=True)
+            return fact
+        except Exception as error:
+            return "Извините, не смог найти интересный факт для вас. Попробуйте ещё раз!"
+
+@loader.tds
+class FactModule(loader.Module):
+    """My module"""
+    strings = {"name": "FactModule", "fact": get_fact()}
+
+    @loader.command
+    async def fact(self, message: Message):
+        """Random fact"""
+        await utils.answer(message, self.strings("fact"))
